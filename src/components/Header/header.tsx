81f02fd1a20c749567c8f86ff7dd5935d652ee9c
Luis Igreja <lfsigreja@gmail.com>
2021-09-08 15:01:10 -0300

    header style

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './style';
+import { NavBar } from './style';
 
 const Header: React.FC = () => {
   return (
-    <Container>
-        
-    </Container>
+    <NavBar>
+        <div className='header-logo'>Logo</div>
+        <div className='header-menu'>
+          <Link to='/' className='header-menu__link'>Inicio</Link>
+          <Link to='/' className='header-menu__link'>Quem Somos</Link>
+          <Link to='/' className='header-menu__link'>Contato</Link>
+          <Link to='/' className='header-menu__link'>Blog</Link>
+          <Link to='/' className='header-menu__link contribute'>
+              Contribua
+            </Link>
+        </div>
+    </NavBar>
   );
 }
 
--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Header from '../../components/Header/header';
 
 import { Container } from './homeStyle';
 
 const Home: React.FC = () => {
   return (
+    <>
+    <Header />
     <Container>
       <h1>Instituto Educar</h1>
     </Container>
+    </>
   )
 }
 

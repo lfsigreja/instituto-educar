fd6dbd30b0f03b29bf7922db24b654c3e274ada5
Luis Igreja <lfsigreja@gmail.com>
2021-09-08 13:38:49 -0300

    setting arquitetura

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Router from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
-    </div>
+    <Router />
   );
 }
 
---
+++ src/views/Home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './homeStyle';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <h1>Instituto Educar</h1>
+    </Container>
+  )
+}
+
+export default Home;

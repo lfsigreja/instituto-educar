d39204914c96ca32b091cd4ecb2e643ed05c8000
Luis Igreja <lfsigreja@gmail.com>
2021-09-08 14:06:25 -0300

    setting arquitetura

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+        
+    </Container>
+  );
+}
+
+export default Header;

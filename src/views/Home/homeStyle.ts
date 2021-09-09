019999cadab81172abeb9f9ba966ec56470c2776
alvaroun <alvaromachadoferreira@hotmail.com>
2021-09-09 13:55:26 -0300

    add home

--- src/views/Home/homeStyle.ts
+++ src/views/Home/homeStyle.ts
@@ -4,8 +4,49 @@ export const Container = styled.main`
   flex-grow: 1;
 
   h1 {
-      font-size: 50px;
-  }
+    /* Instituto Educar */
 
+
+position: absolute;
+width: 596px;
+height: 73px;
+left: 141px;
+top: 380px;
+
+font-family: Asap;
+font-style: normal;
+font-weight: bold;
+font-size: 66px;
+line-height: 76px;
+letter-spacing: 0.1em;
+
+/* Amarelo Vetor */
+
+color: #FFC404;
+
+mix-blend-mode: normal;
+  }
+h3 {
+  /* Espaço de leitura, mediação e formação de leitor. */
+
+
+position: absolute;
+width: 540px;
+height: 25px;
+left: 141px;
+top: 458px;
+
+font-family: IBM Plex Mono;
+font-style: normal;
+font-weight: normal;
+font-size: 18px;
+line-height: 23px;
+
+/* Preto */
+
+color: #1C1C1C;
+
+mix-blend-mode: normal;
+}
   
 `;

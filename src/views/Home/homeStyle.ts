fb56a9208e8141f9b50abb02f1564fb5864e5352
alvaroun <alvaromachadoferreira@hotmail.com>
2021-09-09 14:57:03 -0300

    home page text

--- src/views/Home/homeStyle.ts
+++ src/views/Home/homeStyle.ts
@@ -7,46 +7,69 @@ export const Container = styled.main`
     /* Instituto Educar */
 
 
-position: absolute;
-width: 596px;
-height: 73px;
-left: 141px;
-top: 380px;
+    position: display;
+    width: 596px;
+    height: 73px;
+    left: 141px;
+    top: 380px;
+    margin-left: 6rem;
+
+    font-family: Asap;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 66px;
+    line-height: 76px;
+    letter-spacing: 0.1em;
+
+    /* Amarelo Vetor */
+
+    color: #FFC404;
+
+    mix-blend-mode: normal;
+  }
+h3 {
+    /* Espaço de leitura, mediação e formação de leitor. */
+
+
+    position: display;
+    width: 540px;
+    height: 25px;
+    left: 141px;
+    top: 458px;
+    margin-left: 6rem;
+
+    font-family: IBM Plex Mono;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 23px;
+
+    /* Preto */
+
+    color: #1C1C1C;
+
+    mix-blend-mode: normal;
+}
+p{
+  
+  position: display;
+  width: 603px;
+  height: 309px;
+  left: 145px;
+  top: 557px;
+  margin-left: 6rem;
+
 
 font-family: Asap;
 font-style: normal;
-font-weight: bold;
-font-size: 66px;
-line-height: 76px;
-letter-spacing: 0.1em;
-
-/* Amarelo Vetor */
-
-color: #FFC404;
-
-mix-blend-mode: normal;
-  }
-h3 {
-  /* Espaço de leitura, mediação e formação de leitor. */
-
-
-position: absolute;
-width: 540px;
-height: 25px;
-left: 141px;
-top: 458px;
-
-font-family: IBM Plex Mono;
-font-style: normal;
 font-weight: normal;
-font-size: 18px;
-line-height: 23px;
+font-size: 25px;
+line-height: 47px;
+text-align: justify;
 
 /* Preto */
 
 color: #1C1C1C;
 
-mix-blend-mode: normal;
 }
-  
 `;

03f18d60f6e926d5610e5e1484797760216e65ea
alvaroun <alvaromachadoferreira@hotmail.com>
2021-09-09 21:19:21 -0300

    home update

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,11 +1,82 @@
 import React from 'react';
 import { Container } from './homeStyle';
+import educar2 from '../../assets/educar2.png'
+import edc from '../../assets/edc.png';
+import lta from '../../assets/lta.png';
+import msc from '../../assets/msc.png';
+import dnc from '../../assets/dnc.png';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Instituto Educar</h1>
-      <h3>Espaço de leitura, mediação e formação de leitor.</h3>
+
+
+      <div className="join">
+      <div className="up">
+        <h1>Instituto Educar</h1>
+        <div className='sub-text'>Espaço de leitura, mediação e formação de leitor.</div>
+        <div className='home-text'>
+        <p>O Instituto Educar é um espaço para crianças e jovens unidos pela literatura. Tendo como idealizadora, Catarina Assis, especialista em Educação, Pobreza e Desigualdade Social, bibliotecária, escritora e produtora cultural; o projeto tem por objetivo ser um espaço alternativo para leitura, socialização, criatividade e principalmente, diversão através do universo literário.</p>
+        </div>
+      </div>
+
+      <div className='img-edc'>
+        <img  src={educar2} alt="Instituto Educar"/>
+      </div>
+      </div>
+    <div className='atv'>
+      ATIVIDADES
+    </div>
+    <div className='imgsml'>
+      <div>
+      <img  src={edc} alt="Educação"/>
+      </div>
+      <div>
+      <img  src={lta} alt="Leitura"/>
+      </div>
+      <div>
+      <img  src={msc} alt="Música"/>
+      </div>
+      <div>
+      <img  src={dnc} alt="Dança"/>
+      </div>
+    </div>
+
+    <div className='rentangulo'>
+
+        <div className='cntittle'>NOSSO IMPACTO</div>
+
+      
+      <div className='countn'>
+        <div className='border1'>
+          <h2>
+            200
+          </h2>
+          <h3>
+            Alunos
+          </h3>
+        </div>
+        <div>
+          <h2>
+            70
+          </h2>
+          <h3>
+              Famílias Atendidas
+          </h3>
+        </div>
+        <div  className='border2'>
+          <h2>
+            2000
+          </h2>
+          <h3>
+            Livros Distribuídos
+          </h3>
+        </div>
+      </div>
+    </div>
+    <div className='tltact'>
+      CONTATO
+    </div>
     </Container>
   )
 }
--- src/views/Home/homeStyle.ts
+++ src/views/Home/homeStyle.ts
@@ -4,49 +4,153 @@ export const Container = styled.main`
   flex-grow: 1;
 
   h1 {
-    /* Instituto Educar */
-
-
-position: absolute;
-width: 596px;
-height: 73px;
-left: 141px;
-top: 380px;
-
-font-family: Asap;
-font-style: normal;
-font-weight: bold;
-font-size: 66px;
-line-height: 76px;
-letter-spacing: 0.1em;
-
-/* Amarelo Vetor */
-
-color: #FFC404;
-
-mix-blend-mode: normal;
-  }
-h3 {
-  /* Espaço de leitura, mediação e formação de leitor. */
-
-
-position: absolute;
-width: 540px;
-height: 25px;
-left: 141px;
-top: 458px;
-
-font-family: IBM Plex Mono;
-font-style: normal;
-font-weight: normal;
-font-size: 18px;
-line-height: 23px;
-
-/* Preto */
-
-color: #1C1C1C;
-
-mix-blend-mode: normal;
+    display:flex;
+    justify-content: left;
+    align-items: left;
+    text-align: left;
+    
+    font-family: Asap;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 66px;
+    line-height: 76px;
+    letter-spacing: 0.1em;
+    margin-left: 6rem;
+    color: #FFC404;
 }
+   .home-text {
+    display:flex;
+    text-align: left;
+    width: 603px;
+    height: 362px;
+
+    margin-top: 70px;
+    margin-bottom: 24px;
+    font-family: Asap;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    line-height: 47px;
+    text-align: justify;
+    margin-left: 6rem;
+    color: #1C1C1C;
+  } 
+.sub-text {
+  display:flex;
+  width: 540px;
+  height: 25px;
+  left: 141px;
+  top: 458px;
+  margin-left: 6rem;
+
+  font-family: IBM Plex Mono;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 23px;
   
+}
+.img-edc{
+  display:flex;
+  text-align: left;
+}
+p {
+  margin-bottom: 24px;
+}
+.join {
+  display:flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.up {
+  display:flex;
+  flex-direction: column;
+}
+.atv {
+  margin-top: 220;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Asap", sans-serif;
+  font-weight: 700;
+  text-align: center;
+  font-size: 3rem;
+  letter-spacing: 0.355em;
+  color: #FFC404;
+}
+.imgsml {
+  margin-top: 209;
+  display:flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.cntittle {
+  margin-top: 297;
+  font-family: "Asap";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 55px;
+
+
+  text-align: center;
+  letter-spacing: 0.355em;
+
+
+
+  color: #1C1C1C;
+}
+
+
+.countn {
+  margin-top: 150;
+  font-family: "Asap";
+  font-style: normal;
+  font-weight: 500;
+  text-align: center;
+  display:flex;
+  align-items: center;
+  justify-content: space-around;
+  h2 {
+    font-size: 144px;
+    color:#868686;
+  }
+  h3 { 
+    font-size: 36px;
+    color:#868686;
+  }
+  .border1{
+    border-right : solid 3px #868686;
+    padding: 0 100px
+  }
+  .border2{
+    border-left : solid 3px #868686;
+    padding: 0 100px
+  }
+}
+.namectn {
+  font-size: 36px;
+}
+
+.tltact {
+  margin-top: 220;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Asap", sans-serif;
+  font-weight: 700;
+  text-align: center;
+  font-size: 3rem;
+  letter-spacing: 0.355em;
+  color: #FFC404;
+}
+.retangulo {
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color:#868686 !important;
+  border: 1px solid #EEEEEE;
+  box-sizing: border-box;
+}
 `;
